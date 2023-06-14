f96894dda50b46225faaede850286cb907f119bf
Huy Nguyen <huynp.dev@gmail.com>
2023-06-14 15:33:32 +0700

    update

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Modern Personal Website Template';
-export const SITE_DESCRIPTION = 'Astro Modern Personal Website template with Project Section, CV Section, Paginated Blog, RSS Feed, SEO Friendly, Visual themes and Responsive Desing.';
+export const SITE_TITLE = 'Nguyễn Phát Huy Blog';
+export const SITE_DESCRIPTION = 'Trang cá nhân của Nguyễn Phát Huy - Lập trình viên PHP chuyên nghiệp, có kinh nghiệm trong việc phát triển các trang web và ứng dụng sử dụng PHP, Laravel và Vue.js';
